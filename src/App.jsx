0e7659bd25ef66bb4831d02f64b074b47a4aa85a
m-nelsen <mattnelsen95@gmail.com>
2024-02-23 12:27:08 -0700

    Switching to a typescript project structure

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <p>Vite App</p>
-    </>
-  );
-}
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>Matt Nelsen</h1>
+        <p className="accent">Web Developer</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
